de0c264eae83bc7648a5e993a6783d71d1fb6d23
saintskeeper <walterdayjr@gmail.com>
2024-04-12 23:24:06 -0400

    adding gitop dev

--- tests copy/local-tests.sh
+++
@@ -1,8 +0,0 @@
-docker run -d -p 8000:8000 my-go-app
-
-
-curl http://localhost:8000/items
-
-curl -X POST http://localhost:8000/items -d '{"id":"1", "name":"Item 1"}' -H "Content-Type: application/json"
-
-curl http://localhost:8000/items
